46bb7128428846358c8431996fac735593b3d4ac
likhithashylu <likitha.shylu@gmail.com>
2024-10-03 14:39:37 +0530

    [LIKHITHA] add-Applying logic to use the match cases to call methods.

--- EmpWageCompute.py
+++ EmpWageCompute.py
@@ -52,7 +52,32 @@ def parttime_emp_wage(no_of_hours, per_hour_wage ):
     total_wages=no_of_hours*per_hour_wage
     print(f"parttime wage do employee get for his/her work will be: {total_wages} rupees")
     
+    
+def match_cases():
+    """
+        
+        Description: 
+           This function is used to match the test cases according to user's requirement.
+        Parameters:
+           choice is a container to hold the user input .
+        Return:
+            It prints the output according to the given choice . 
+        
+    """
+    choice=int(input("Enter the choices between 1 to 3:"))
+    match choice:
+        case 1:
+            check_attendance(random.randint(0, 1))
+         
+        case 2:
+            Emp_Daily_Wage(20, 8)
+        case 3:
+            parttime_emp_wage(4, 8)
+
+match_cases()
+
 def main():
+    print("--------------------")
     # Check attendance
     attended = random.randint(0, 1)
     check_attendance(attended)
