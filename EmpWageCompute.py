e11d2ab9fa0d65ba6f66717675924c6cabc9557c
likhithashylu <likitha.shylu@gmail.com>
2024-10-03 14:21:07 +0530

    [LIKHITHA] add-Applying logic to calculate the employee daily and parttime wages with there attendance track.

--- EmpWageCompute.py
+++ EmpWageCompute.py
@@ -37,6 +37,21 @@ def Emp_Daily_Wage(wage_per_hour, no_of_hour):
     total_earning = wage_per_hour * no_of_hour
     print(f"Daily wage that an employee will earn: {total_earning} rupees")
 
+def parttime_emp_wage(no_of_hours, per_hour_wage ):
+    """
+        
+        Description: 
+           This function is used to calculate the part time wage of an employees.
+        Parameters:
+           no_of_hours, per_hour_wage is a parameter that used to check the condition
+        Return:
+            It prints the output according to the calculation . 
+        
+    """
+    
+    total_wages=no_of_hours*per_hour_wage
+    print(f"parttime wage do employee get for his/her work will be: {total_wages} rupees")
+    
 def main():
     # Check attendance
     attended = random.randint(0, 1)
@@ -47,5 +62,10 @@ def main():
     no_of_hour = 8
     Emp_Daily_Wage(wage_per_hour, no_of_hour)
 
+    # Calculate parttime wage
+    no_of_hours = 4
+    per_hour_wage = 18
+    parttime_emp_wage(no_of_hours, per_hour_wage )
+    
 if __name__ == "__main__":
     main()
